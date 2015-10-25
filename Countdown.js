0e07ef0f1c1ee6621672f85bb66ba1c98b06efbe
Balint Soos <soosbalint95@gmail.com>
2015-10-25 22:57:30 +0100

    Initial commit

---
+++ Countdown.js
@@ -0,0 +1,58 @@
+
+function Countdown (date) {
+  var Countdown = {};
+  
+  Countdown.end = new Date(date);
+
+  Countdown.start = function () {
+    function Clock () {
+      var now = new Date();
+      var end = Countdown.end;
+      var remaining = end.getTime() - now.getTime();
+      dateDisplay(dateConversion(remaining));
+      
+      setTimeout(Clock, 1000);
+
+      function dateConversion (t) {
+        var cd = 24 * 60 * 60 * 1000,
+            ch = 60 * 60 * 1000,
+            cm = 60 * 1000,
+            d = Math.floor(t / cd),
+            h = Math.floor( (t - (d * cd)) / ch),
+            m = Math.floor( (t - (d * cd) - (h * ch)) / cm),
+            s = Math.floor( (t - (d * cd) - (h * ch) - (m * cm)) / 1000);
+        
+        if (s === -1) {
+          m--;
+          s = 59;
+        }
+        
+        if (m === -1) {
+          h--;
+          m = 59;
+        }
+        
+        if (h === -1) {
+          d--;
+          h = 23;
+        }
+        
+        return {
+          days: d,
+          hours: h,
+          minutes: m,
+          seconds: s
+        };
+      }
+      function dateDisplay (dateObject) {
+        document.getElementById('days').innerHTML = dateObject.days;
+        document.getElementById('hours').innerHTML = dateObject.hours;
+        document.getElementById('minutes').innerHTML = dateObject.minutes;
+        document.getElementById('seconds').innerHTML = dateObject.seconds;
+      }
+    }
+    Clock();
+  }
+
+  return Countdown;
+}
---
+++ app.js
@@ -0,0 +1,4 @@
+
+var StarWars = new Countdown("December 18, 2015 00:00:01");
+
+StarWars.start();
